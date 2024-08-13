8e8aa8b9d64c191ad06fea50fedf39b05804bbd1
hasubinvj <subin396@naver.com>
2024-08-13 15:33:26 +0900

    first commit

---
+++ Fristjava/src/Hello.java
@@ -0,0 +1,14 @@
+
+/*
+ 다중 행 주석 (여러 줄 기입)
+ */
+
+//  //만나는 부분 부터 주석으로 인식
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("자바는 처음이지 "); //화면상 출력해 주는 부분
+	}
+
+}
---
+++ Java20240813/src/ex3/자동캐스트변환.java
@@ -0,0 +1,15 @@
+package ex3;
+
+/*
+ *  수동 형변환 or 강제 형변환
+ *  큰 자료형 +> 작은 자료형 대입한다
+ */
+public class 자동캐스트변환 {
+
+	public static void main(String[] args) {
+		
+	
+
+	}
+
+}
